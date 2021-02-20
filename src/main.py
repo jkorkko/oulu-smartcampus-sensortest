4a45593d407379867abc7924cc17540b9d9f707d
aleksipirttimaa <4463880+aleksipirttimaa@users.noreply.github.com>
2021-02-20 12:21:51 +0200

    Add helpers for Smart Campus API and Api-Key authentication

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import csv
-import influxdb
 import warnings
-import requests
+
 import configparser
+import influxdb
+import requests
+
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 
@@ -10,15 +12,74 @@ CONF_FILE = "settings.conf"
 config = configparser.ConfigParser()
 config.read(CONF_FILE)
 
-DB_USER = config.get("info", "user")
-DB_PASS = config.get("info", "pass")
-DB_HOST = config.get("info", "host")
-DB_DEFAULT = config.get("info", "db_default")
-DB_PORT = config.get("HTTP", "port")
+DB_USER = config.get("InfluxDb", "user")
+DB_PASS = config.get("InfluxDb", "pass")
+DB_HOST = config.get("InfluxDb", "host")
+DB_DEFAULT = config.get("InfluxDb", "db_default")
+DB_PORT = config.get("InfluxDb", "port")
 CLIENT = influxdb.InfluxDBClient(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_DEFAULT)
+
+SC_ENDPOINT = config.get("SmartCampusApi", "url").rstrip("/")
+# Optional ApiKey obtained from the api. If key is None, skip authenticated requests.
+SC_APIKEY = config.get("SmartCampusApi", "apikey") or None
+if not SC_APIKEY:
+    warnings.warn("Updating sensors is disabled, as apikey is not specified in settings")
 
 csv_url = config.get("URL", "all")
 online_query = config.get("Query", "deveuis")
+
+
+def smart_campus_devices():
+    '''
+    Fetch all devices from the Smart Campus Api.
+
+    Returns:
+        List of device objects.
+
+    Raises:
+        RuntimeError: Bad response or no devices.
+    '''
+    response = requests.get(F"{SC_ENDPOINT}/devices/listAll")
+    if response.status_code != 200:
+        content = response.content.decode("utf-8")
+        raise RuntimeError(F"Smart Campus API responded with status {response.status_code}: {content}")
+    devices = response.json()
+    if not isinstance(devices, list) or devices == []:
+        raise RuntimeError("Smart Campus API didn't respond with devices.")
+    return devices
+
+def smart_campus_update(_id, status):
+    '''
+    Update a sensor's status by sending a POST-request to the Smart Campus API.
+
+    Args:
+        _id: smart campus device document _id
+        status: status string
+
+    Returns:
+        None
+
+    Raises:
+        RuntimeError: Device couldn't be updated.
+    '''
+    if not SC_APIKEY:
+        return
+
+    headers = {"Authorization": F"Api-Key {SC_APIKEY}"}
+    update = {"status": status}
+    data = {"_id": _id, "update": update}
+
+    response = requests.post(F"{SC_ENDPOINT}/devices/update", json=data, headers=headers)
+    if response.status_code != 200:
+        content = response.content.decode("utf-8")
+        raise RuntimeError(F"Smart Campus API responded with status {response.status_code}: {content}")
+
+    response_json = response.json()
+    if not isinstance(response_json, dict):
+        raise RuntimeError("Device update got bad response from Smart Campus API.")
+    if "success" not in response_json or not response_json["success"]:
+        message = response_json["msg"] if "msg" in response_json else ""
+        raise RuntimeError(F"Device update failed: {message}")
 
 
 def check_devices():
@@ -37,9 +98,9 @@ def check_devices():
 
 def valid_deveuis():
     '''
-    Return a list of all devices we have
+    Return a list of all devices we have.
 
-    Fetches static-sensors/all.csv or other configured .csv using http
+    Fetches static-sensors/all.csv or other configured .csv using http.
     '''
     deveuis = []
     with requests.Session() as sensors:
